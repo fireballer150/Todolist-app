5170e1b77ad3f9e3ac506c1edb4bbe922d4b3d7a
fireballer150 <cutterballer@gmail.com>
2022-10-11 00:23:55 +0900

    Todo-app complete

---
+++ src/components/TodoList.js
@@ -0,0 +1,19 @@
+import TodoListItem from './TodoListItem';
+import './TodoList.scss';
+
+const TodoList = ({ todos, onRemove, onToggle }) => {
+  return (
+    <div className="TodoList">
+      {todos.map((todo) => (
+        <TodoListItem
+          todo={todo}
+          onRemove={onRemove}
+          onToggle={onToggle}
+          key={todo.id}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
